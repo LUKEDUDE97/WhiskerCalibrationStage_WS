85e2bbaa59fa902e6976daa2da26367b0e5c2233
lukedude97 <dyx4work@gmail.com>
2023-10-26 11:34:10 +0200

    add MagneticFieldVector.msg and update MagneticSensor node

--- src/tle493dw2b6_nano_processor/sketch/sketch.ino
+++ src/tle493dw2b6_nano_processor/sketch/sketch.ino
@@ -1,42 +1,55 @@
-#include <Tle493d_w2b6.h>
+//  ~/Arduino/libraries/<PackageName>/src
 
 #include <ros.h>
 #include <std_msgs/Float32.h>
-// #include <tle493dw2b6_nano_processor/MagneticFieldVector.h>
+#include <Tle493d_w2b6.h>
+#include <tle493dw2b6_nano_processor/MagneticFieldVector.h>
 
 // Create instances of the Tle493d_w2b6 class
 Tle493d_w2b6 Tle493dMagnetic3DSensor1 = Tle493d_w2b6(Tle493d::FASTMODE); // Address 0x35
 
 // Set up the ros node and publisher
-std_msgs::Float32 msg_x;
+tle493dw2b6_nano_processor::MagneticFieldVector FieldVector_msg;
 
 ros::NodeHandle nh;
-ros::Publisher pub("MagneticSensor", &msg_x);
+ros::Publisher pub("MagneticSensor", &FieldVector_msg);
 
-void setup() {
+void setup()
+{
 
-  nh.initNode();
-  nh.advertise(pub);
+    nh.initNode();
+    nh.advertise(pub);
+    
+    // Initialize one sensor
+    Tle493dMagnetic3DSensor1.begin();
 
-  // Initialize one sensor
-  Tle493dMagnetic3DSensor1.begin();
+    int thr = 2;
+    Tle493dMagnetic3DSensor1.setWakeUpThreshold(thr, -(thr), thr, -(thr), thr, -(thr));
 
-  int thr= 2;
-  Tle493dMagnetic3DSensor1.setWakeUpThreshold(thr,-(thr),thr,-(thr),thr,-(thr));
-
-  //The update rate is set to 3 (fastest is 0 and slowest is 7)
-  // Tle493dMagnetic3DSensor1.setUpdateRate(3);
-
+    // The update rate is set to 3 (fastest is 0 and slowest is 7)
+    //  Tle493dMagnetic3DSensor1.setUpdateRate(7);
 }
 
-void loop() {
-  // Update data from sensors
-  Tle493dMagnetic3DSensor1.updateData();
+long publish_timer;
 
-  msg_x.data = Tle493dMagnetic3DSensor1.getX();
-  pub.publish(&msg_x);
+void loop()
+{
 
-  nh.spinOnce();
-  delay(100);
+    if (millis() > publish_timer)
+    {
 
+        // Request readings from sensor
+        Tle493dMagnetic3DSensor1.updateData();
+        delay(10);
+
+        // FieldVector_msg.data = Tle493dMagnetic3DSensor1.getX();
+        FieldVector_msg.magnetic_x = Tle493dMagnetic3DSensor1.getX();
+        FieldVector_msg.magnetic_y = Tle493dMagnetic3DSensor1.getY();
+        FieldVector_msg.magnetic_z = Tle493dMagnetic3DSensor1.getZ();
+        pub.publish(&FieldVector_msg);
+
+        publish_timer = millis() + 500; // publish once a second
+    }
+
+    nh.spinOnce();
 }
